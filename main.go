800f23b98e2bd907be17c02099bcac575d6ee935
maxcleme <maxime.clement@docker.com>
2024-04-03 12:18:41 +0200

    fix: api
    Signed-off-by: maxcleme <maxime.clement@docker.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -15,9 +16,8 @@ var ctx = context.Background()
 func main() {
 	// Initialize a Redis client.
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_HOST"), // Redis server address host:port
-		Password: os.Getenv("REDIS_PASSWORD"),
-		DB:       0, // Default DB
+		Addr: os.Getenv("REDIS_HOST"), // Redis server address host:port
+		DB:   0,                       // Default DB
 	})
 
 	// Initialize Gin.
@@ -28,11 +28,13 @@ func main() {
 
 		// Retrieve the updated visitor count.
 		visitors, err := rdb.Get(ctx, "visitors").Result()
-		if err != nil {
+		if err != nil && !errors.Is(err, redis.Nil) {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving visitor count"})
 			return
 		}
-
+		if visitors == "" {
+			visitors = "0"
+		}
 		// Send a response to the client.
 		c.String(http.StatusOK, fmt.Sprintf("This page has been visited %s time(s)", visitors))
 	})
